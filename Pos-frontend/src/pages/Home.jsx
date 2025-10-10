37f7661e91300e7615dcf5e3ea67e82f36b18e5d
Mubashir <157006840+IamMubashir@users.noreply.github.com>
2025-10-10 16:30:34 +0500

    frontend working

---
+++ Pos-frontend/src/pages/Home.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import BottomNav from "../components/BottomNav";
+import Greetings from "../components/Home/Greetings";
+import { BsCashCoin } from "react-icons/bs";
+import { GrInProgress } from  "react-icons/gr";
+import MiniCard from "../components/Home/MiniCard";
+function Home() {
+  return (
+    <section className="bg-[#1f1f1f] h-[calc(100vh-5rem)] overflow-hidden flex gap-3">
+      {/*LeFT DIV*/}
+<div className="flex-[3] bg-[#1a1a1a]">
+<Greetings/>
+<div className="flex items-center w-full gap-3 px-8 mt-8">
+  <MiniCard title="Total Earnings" icon={<BsCashCoin/>} number={512} footerNum={1.6} />
+  <MiniCard title="In Progress" icon={<GrInProgress/>} number={512} footerNum={3.6} />
+</div>
+</div>
+      {/*RIGHT DIV*/}
+      <div className="flex-[2] bg-[#1a1a1a]">
+<BottomNav/>
+      </div>
+    </section>
+  );
+}
+
+export default Home;
